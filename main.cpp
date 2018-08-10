b50786f486617d0027af5a9fbf67db07ffb145a3
Alexandre Chariot <alexandre.chariot@cea.fr>
2018-08-10 08:57:46 +0200

    Chap 12 : Random scene

--- main.cpp
+++ main.cpp
@@ -28,33 +28,73 @@ vec3 color(const ray& r, Hitable* world, int depth)
     }
 }
 
+Hitable* random_scene()
+{
+    int n = 500;
+    Hitable** list = new Hitable*[n + 1];
+    list[0] = new Sphere(vec3(0, -1000, 0), 1000, new Lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new Sphere(center, 0.2, new Lambertian(vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                } else if (choose_mat < 0.95) {  // metal
+                    list[i++] = new Sphere(center, 0.2,
+                                           new Metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * drand48()));
+                } else {  // glass
+                    list[i++] = new Sphere(center, 0.2, new Dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new Sphere(vec3(0, 1, 0), 1.0, new Dielectric(1.5));
+    list[i++] = new Sphere(vec3(-4, 1, 0), 1.0, new Lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new Sphere(vec3(4, 1, 0), 1.0, new Metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new Hitable_list(list, i);
+}
+
 int main()
 {
     std::ofstream file_pgm;
     file_pgm.open("test.pgm");
 
-#if 1
+#if 0
     int nx = 400;
     int ny = 200;
     int ns = 100;
 #else
-    int nx = 1200;
-    int ny = 600;
-    int ns = 200;
+    int nx = 1920;  // 1200;
+    int ny = 1080;  // 600;
+    int ns = 400;   // 200;
+
 #endif
     file_pgm << "P3\n" << nx << " " << ny << "\n255\n";
-    Hitable* list[5];
-    list[0] = new Sphere(vec3(0, 0, -1), 0.5, new Lambertian(vec3(0.1, 0.2, 0.5)));
-    list[1] = new Sphere(vec3(0, -100.5, -1), 100, new Lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new Sphere(vec3(1, 0, -1), 0.5, new Metal(vec3(0.8, 0.6, 0.2), 0.1));
-    list[3] = new Sphere(vec3(-1, 0, -1), 0.5, new Dielectric(1.5));
-    list[4] = new Sphere(vec3(-1, 0, -1), -0.45, new Dielectric(1.5));
-    Hitable* world = new Hitable_list(list, 5);
 
-    vec3 lookfrom(3, 3, 2);
-    vec3 lookat(0, 0, -1);
-    float dist_to_focus = (lookfrom - lookat).length();
-    float aperture = 2.0;
+    // Hitable* list[5];
+    // list[0] = new Sphere(vec3(0, 0, -1), 0.5, new Lambertian(vec3(0.1, 0.2, 0.5)));
+    // list[1] = new Sphere(vec3(0, -100.5, -1), 100, new Lambertian(vec3(0.8, 0.8, 0.0)));
+    // list[2] = new Sphere(vec3(-1, 0, -1), 0.5, new Metal(vec3(0.8, 0.6, 0.2), 0.1));
+    // list[3] = new Sphere(vec3(1, 0, -1), 0.5, new Dielectric(1.5));
+    // list[4] = new Sphere(vec3(1, 0, -1), -0.45, new Dielectric(1.5));
+    // Hitable* world = new Hitable_list(list, 5);
+
+    // vec3 lookfrom(3, 3, 2);
+    // vec3 lookat(0, 0, -1);
+    // float dist_to_focus = (lookfrom - lookat).length();
+    // float aperture = 0.1;
+    // Camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
+
+    Hitable* world = world = random_scene();
+
+    vec3 lookfrom(13, 2, 3);
+    vec3 lookat(0, 0, 0);
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
     Camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
 
 #ifdef MONITOR_TIME
